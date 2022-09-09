dd10e8c26db8eabf5c1a026a7c6f1f34d0c08cbd
Naughty-1 <gamesnoble47@gmail.com>
2022-09-09 14:15:52 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function 
-*
-* Description: using the main function 
-* this program prints "Programming is like building a multilingual puzzle
-* Return:0
-*/
+ *
+ * * main-entry point for the program.
+ *
+ * *
+ *
+ * * Return:0 if no errors, return non zero if errors
+ *
+ * */
+
 int main(void)
+
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
 }
-^C
+
+
