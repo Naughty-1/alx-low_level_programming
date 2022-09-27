1686d5143a7a74093ce12db9a2e68074f9e9067b
Naughty-1 <gamesnoble47@gmail.com>
2022-09-26 19:19:21 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*_memset - fills a memory block with a constant byte
-*@s: address to memory block 
-*@b: char to be used
-*@n: number of bytes to be used 
+* _memset - fills a memory block with a constant byte
+* @s: address to memory block 
+* @b: char to be used
+* @n: number of bytes to be used 
 *
-* Return: poiner to the memory block 
+* Return: pointer to the memory block 
 */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -15,6 +15,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[n - 1] = b;
 		n--;
-  	}		
+	}		
 	return (s);
 }
