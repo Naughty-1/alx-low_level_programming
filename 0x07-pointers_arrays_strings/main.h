0ec5c277f69b9aa18592c15ad39c90537ba1ed14
Naughty-1 <gamesnoble47@gmail.com>
2022-09-27 04:41:20 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,17 +2,12 @@
 #define MAIN_H
 
 /**
+* void prototypes()
+* int prototypes()
+*/
 
- * File: main.h
- * Auth: sam tech (samuel birhanu)
- * description: Header file containing prototypes for all functions
- *         used in the 0x06-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -22,4 +17,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif /*MAIN_H*/
